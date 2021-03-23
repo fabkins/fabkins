af829bef6d8f4111b0e80e2d0c763d736343c881
fabkins <fabrice.helliker@gmail.com>
2021-03-23 22:44:29 +0000

    commit

--- src/schedule_pane.js
+++ src/schedule_pane.js
@@ -1,28 +1,3 @@
-/*
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import { useDemoData } from '@material-ui/x-grid-data-generator';
-
-//export default function SingleRowSelectionGrid() {
-
-function SingleRowSelectionGrid() {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 10,
-    maxColumns: 6,
-  });
-
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid {...data} />
-    </div>
-  );
-}
-
-
-export default SingleRowSelectionGrid;
-
-*/
 
 import React from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
